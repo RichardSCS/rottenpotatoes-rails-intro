e7da65e88202592fd79410efff8ea32289150872
Richard Cruz-Silva <rsc2174@columbia.edu>
2022-10-05 07:17:14 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,26 +9,40 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.all_ratings.sort
 
-    @ratings_to_show = []
-
-    @ratings_to_show = params[:ratings].keys unless params[:ratings] == nil
+    if session[:ratings] == nil
+      @ratings_to_show = @all_ratings
+    elsif params[:commit] == "Refresh" || !(params[:sort] == nil)
+      if params[:ratings] == nil
+        @ratings_to_show = []
+      else
+        @ratings_to_show = params[:ratings].keys
+      end
+    else
+      @ratings_to_show = session[:ratings].keys
+    end
 
     @movies = Movie.with_ratings(@ratings_to_show)
 
     @ratings_hash = Hash.new
     @ratings_to_show.each { |r| @ratings_hash["#{r}"] = "1"}
 
-    case params[:sort]
+    if !(params[:sort] == nil)
+      @current = params
+    elsif !(session[:sort] == nil)
+      @current = session
+    end
+
+    case @current[:sort]
     when 'title'
       @hightlight_title_header = true
     when 'release_date'
       @hightlight_release_date_header = true
     end
-    @movies = @movies.order(params[:sort])
+    @movies = @movies.order(@current[:sort])
 
-#    session[:ratings] = params[:ratings] unless params[:commit] == nil
- #   session[:sort] = params[:sort]
-#    flash[:notice] = "#{params}"
+    session[:ratings] = params[:ratings] unless params[:commit] == nil
+    session[:sort] = params[:sort] unless params[:sort] == nil
+    flash[:notice] = "#{params}"
   end
 
   def new
